170fd62ea80e831c079454fdd42e8bda91592866
MartoDD <zail_mdd@hotmail.com>
2022-04-03 12:17:20 +0300

    Programming Fundamentals with Java

--- Programming Fundamentals with Java/src/MapsLambdaAndStreamAPI/MoreExercises/Snowwhite.java
+++ Programming Fundamentals with Java/src/MapsLambdaAndStreamAPI/MoreExercises/Snowwhite.java
@@ -1,6 +1,8 @@
 package MapsLambdaAndStreamAPI.MoreExercises;
 
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Snowwhite {
     public static void main(String[] args) {
@@ -8,72 +10,47 @@ public class Snowwhite {
         Scanner scanner = new Scanner(System.in);
 
         String input = scanner.nextLine();
-        Map<String, List<Dwarf>> dwarves = new LinkedHashMap<>();
+        Map<String, LinkedHashMap<String, Integer>> dwarves = new LinkedHashMap<>();
 
         while (!input.equals("Once upon a time")) {
             String[] data = input.split(" <:> ");
             String name = data[0];
             String color = data[1];
             int physics = Integer.parseInt(data[2]);
-            boolean dwarfIsPresent = false;
-            if (!dwarves.containsKey(color)){
-                dwarves.putIfAbsent(color,new ArrayList<>());
-                dwarves.get(color).add(new Dwarf(name,physics));
-            }else {
-                for (Dwarf dwarf : dwarves.get(color)) {
-                    if (dwarf.getName().equals(name)){
-                        dwarfIsPresent=true;
-                        if (dwarf.getPhysics()<physics){
-                            dwarf.setPhysics(physics);
-                            break;
-                        }
-                    }
-                }
-                if (!dwarfIsPresent){
-                    dwarves.get(color).add(new Dwarf(name,physics));
-                }
 
+            if (dwarves.containsKey(color) && dwarves.get(color).containsKey(name)) {
+                if (dwarves.get(color).get(name) < physics) {
+                    dwarves.get(color).put(name, physics);
+                }
+            } else if (dwarves.containsKey(color) && !dwarves.get(color).containsKey(name)) {
+                dwarves.get(color).put(name, physics);
+            } else {
+                dwarves.putIfAbsent(color, new LinkedHashMap<>());
+                dwarves.get(color).putIfAbsent(name, physics);
             }
+
 
             input = scanner.nextLine();
         }
 
-        dwarves.entrySet()
-                .stream()
-                .sorted((d1, d2) -> {
-                    int result = Integer.compare(d2.getValue().stream().mapToInt(Dwarf::getPhysics).sum(), d1.getValue().stream().mapToInt(Dwarf::getPhysics).sum());
-                    if (result == 0) {
-                        result = Integer.compare(d1.getValue().size(), d2.getValue().size());
-                    }
-                    return result;
-                }).forEach(stringListEntry -> stringListEntry.getValue().forEach(dwarf -> System.out.printf("(%s) %s <-> %d%n", stringListEntry.getKey(), dwarf.getName(), dwarf.getPhysics())));
+        Map<String, Integer> print = new LinkedHashMap<>();
 
-
-    }
-
-    static class Dwarf {
-        String name;
-        int physics;
-
-        public String getName() {
-            return name;
+        for(Map.Entry<String, LinkedHashMap<String, Integer>> entry : dwarves.entrySet()) {
+            for(Map.Entry<String, Integer> subEntry : entry.getValue().entrySet()) {
+                print.put(entry.getKey() + " " + subEntry.getKey() + " " + entry.getValue().size(), subEntry.getValue());
+            }
         }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public int getPhysics() {
-            return physics;
-        }
-
-        public void setPhysics(int physics) {
-            this.physics = physics;
-        }
-
-        public Dwarf(String name, int physics) {
-            this.name = name;
-            this.physics = physics;
-        }
+        print.entrySet().stream().sorted((pair2, pair1) -> {
+            int sort = Integer.compare(pair1.getValue(), pair2.getValue());
+            if (sort == 0) {
+                String[] sort1 = pair1.getKey().split(" ");
+                String[] sort2 = pair2.getKey().split(" ");
+                sort = sort1[2].compareTo(sort2[2]);
+            }
+            return sort;
+        }).forEach(pair -> {
+            String[] printing = pair.getKey().split(" ");
+            System.out.printf("(%s) %s <-> %d\n", printing[0], printing[1], pair.getValue());
+        });
     }
 }
