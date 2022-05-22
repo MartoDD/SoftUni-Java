e757a23252a6fa3203bb32a10887b1af1ccede59
MartoDD <zail_mdd@hotmail.com>
2022-05-22 20:40:54 +0300

    Java Advanced

--- Professional Modules/Java Advanced/Java Advanced/src/MultidimensionalArrays/Exercises/_1_FillTheMatrix.java
+++ Professional Modules/Java Advanced/Java Advanced/src/MultidimensionalArrays/Exercises/_1_FillTheMatrix.java
@@ -8,44 +8,61 @@ public class _1_FillTheMatrix {
         Scanner scanner = new Scanner(System.in);
 
         String[] input = scanner.nextLine().split(", ");
-        int value=Integer.parseInt(input[0]);
-        switch (input[1]) {
-            case "A":
-                printMatrixA(value);
-                break;
-            case "B":
-                printMatrixB(value);
-                break;
-        }
-
-
-    }
-
-    private static void printMatrixB(int value) {
-
-        int[][] matrix=new int[value][value];
-        for (int row = 0; row < value; row++) {
-            int number=row+1;
-            int previousNumber=0;
-            for (int col = 0; col < value; col++) {
-                if (col%2==0){
-                    matrix[row][col]=number+previousNumber;
-                }else {
-
-                }
-                previousNumber=matrix[row][col];
+        int value = Integer.parseInt(input[0]);
+        if (value == 0) {
+            System.out.println(0);
+        } else {
+            switch (input[1]) {
+                case "A":
+                    printMatrixA(value);
+                    break;
+                case "B":
+                    printMatrixB(value);
+                    break;
             }
         }
     }
 
+    private static void printMatrixB(int value) {
+
+        int[][] matrix = new int[value][value];
+        for (int row = 0; row < value; row++) {
+            int number = row + 1;
+            int toAdd = value * 2;
+            for (int col = 0; col < value; col++) {
+                if (col % 2 == 0) {
+                    if (col == 0) {
+                        matrix[row][col] = number;
+                    } else {
+                        matrix[row][col] = matrix[row][col - 2] + toAdd;
+                    }
+                } else {
+                    if (col == 1) {
+                        matrix[row][col] = matrix[row][col - 1] + toAdd - number - row;
+                    } else {
+                        matrix[row][col] = matrix[row][col - 2] + toAdd;
+                    }
+                }
+            }
+        }
+        for (int[] row : matrix) {
+            for (int col : row) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+
+        }
+
+    }
+
     private static void printMatrixA(int value) {
 
-        int[][] matrix=new int[value][value];
+        int[][] matrix = new int[value][value];
         for (int row = 0; row < value; row++) {
-            int number=row+1;
+            int number = row + 1;
             for (int col = 0; col < value; col++) {
-                matrix[row][col]=number;
-                number+=value;
+                matrix[row][col] = number;
+                number += value;
             }
 
         }
